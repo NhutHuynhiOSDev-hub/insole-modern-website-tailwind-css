5477130988d2b835bb8b4d15a252ac2b211cfde8
nhuthq <nhuthq@zigexn.vn>
2024-11-15 14:43:10 +0700

    init source

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-pink-200 py-8 lg:pt-6 lg:pb-14">
+      <div className="container mx-auto lg:relative flex flex-col lg:flex-row lg:justify-between gap-4 lg:gap-0">
+        Header
+      </div>
+    </header>
+  );
+};
+
+export default Header;
