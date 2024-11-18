243d8b16fb6a75bc4b0ebadbde9b2cd1277232fc
nhuthq <nhuthq@zigexn.vn>
2024-11-18 15:18:46 +0700

    finish section

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import FAQ from "./FAQ";
 import Hero from "./Hero";
 import Team from "./Team";
+import Blogs from "./Blogs";
 import Stats from "./Stats";
 import Header from "./Header";
 import Services from "./Services";
@@ -14,6 +15,7 @@ export {
   Team as TeamSection,
   Hero as HeroSection,
   Stats as StatsSection,
+  Blogs as BlogsSection,
   Services as ServicesSection,
   Department as DepartmentSection,
   Appointment as AppointmentSection,
